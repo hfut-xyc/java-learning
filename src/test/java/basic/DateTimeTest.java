5fb082b07aefd993d3d5914c869a6c2d9572b0fb
hfut-xyc <hfut.xieyincen@foxmail.com>
2022-09-11 10:29:40 +0800

    DateTimeTest updated

--- src/test/java/basic/DateTimeTest.java
+++ src/test/java/basic/DateTimeTest.java
@@ -2,6 +2,7 @@ package basic;
 
 import org.junit.Test;
 
+import java.text.ParseException;
 import java.text.SimpleDateFormat;
 import java.time.LocalDate;
 import java.time.LocalDateTime;
@@ -15,42 +16,37 @@ import java.util.Date;
 public class DateTimeTest {
 
     @Test
-    public void test1() {
-        Date date = new Date();
+    public void test1() throws ParseException {
+        SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd");
+        System.out.println(sdf.format(new Date()));
+        System.out.println(sdf.parse("2022-09-10"));
 
-        SimpleDateFormat sdf1 = new SimpleDateFormat("yyyy-MM-dd");
-        System.out.println(sdf1.format(date));
-
-        SimpleDateFormat sdf2 = new SimpleDateFormat("HH:mm:ss");
-        System.out.println(sdf2.format(date));
-
-        SimpleDateFormat sdf3 = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
-        System.out.println(sdf3.format(date));
+        DateTimeFormatter dtf = DateTimeFormatter.ofPattern("yyyy-MM-dd");
+        System.out.println(LocalDateTime.now().format(dtf));
+        System.out.println(dtf.parse("2022-09-10"));
     }
 
     @Test
     public void test2() {
         LocalDate date = LocalDate.now();
-        System.out.println(date);
-
         LocalTime time = LocalTime.now();
-        System.out.println(time);
-
         LocalDateTime dateTime = LocalDateTime.now();
-        DateTimeFormatter dtf = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss");
-        System.out.println(dateTime.format(dtf));
-    }
+        System.out.println(date);
+        System.out.println(time);
+        System.out.println(dateTime);
 
-    @Test
-    public void test3() {
-        LocalDate date1 = LocalDate.of(2022, 1, 14);
-        LocalDate date2 = LocalDate.parse("2022-01-14");
-
+        LocalDate date1 = LocalDate.of(2022, 9, 10);
         LocalTime time1 = LocalTime.of(12, 10, 22);
+        LocalDateTime dateTime1 = LocalDateTime.of(2022, 9, 10, 12, 10, 22);
+        System.out.println(date1);
+        System.out.println(time1);
+        System.out.println(dateTime1);
+
+        LocalDate date2 = LocalDate.parse("2022-09-10");
         LocalTime time2 = LocalTime.parse("12:10:22");
-
-        LocalDateTime dateTime1 = LocalDateTime.of(2022, 1, 14, 12, 10, 22);
-        LocalDateTime dateTime2 = LocalDateTime.parse("2022-01-14 12:10:22");
+        LocalDateTime dateTime2 = LocalDateTime.parse("2022-09-10T12:10:22");
+        System.out.println(date2);
+        System.out.println(time2);
+        System.out.println(dateTime2);
     }
-
 }
